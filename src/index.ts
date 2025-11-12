ec39c6a8c4ccec7e8235fda0de16a5dc753a83d5
PedroML71 <pedrolimafsa@hotmail.com>
2025-11-11 22:41:05 -0300

    backend setup

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import * as dynamoose from "dynamoose";
+// ROUTE IMPORTS
+
+// CONFIGURATIONS
+const isProduction = process.env.NODE_ENV === "production";
+
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
+}
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// SERVER
+const port = process.env.PORT || 3000;
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
