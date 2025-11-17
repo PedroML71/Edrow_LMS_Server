fe0bcef9cc986251e1ca244333dad965bbc9b15e
PedroML71 <pedrolimafsa@hotmail.com>
2025-11-17 13:58:32 -0300

    add user clerk endpoint

---
+++ src/controllers/userClerkController.ts
@@ -0,0 +1,23 @@
+import { clerkClient } from "../index";
+import type { Request, Response } from "express";
+
+export const updateUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.params;
+  const userData = req.body;
+
+  try {
+    const user = await clerkClient.users.updateUserMetadata(userId as string, {
+      publicMetadata: {
+        userType: userData.publicMetadata.userType,
+        settings: userData.publicMetadata.settings,
+      },
+    });
+
+    res.json({ message: "User updated successfully", data: user });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user", error });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import { createClerkClient } from "@clerk/express";
 // ROUTE IMPORTS
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 // CONFIGURATIONS
 const isProduction = process.env.NODE_ENV === "production";
@@ -12,6 +14,10 @@ const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -27,6 +33,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", userClerkRoutes);
 
 // SERVER
 const port = process.env.PORT || 3000;
